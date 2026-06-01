e027d7d81eec97c26e9901cd058ad67a149a7cfb
NrAOmar <omarfahmy1006@gmail.com>
2026-06-01 09:51:55 +0200

    Apply it in simulink

--- Balancing_Robot.m
+++ Balancing_Robot.m
@@ -1,4 +1,4 @@
-% clear all, clc
+clear all, clc
 
 %% Initial Conditions
 
@@ -173,12 +173,6 @@ acc_nl = simplify(D \ (-Cg + u));   % n x 1 symbolic q_ddot expressions
 toc
 tic
 
-K = zeros(1, 4, round((pi/180 / pi/2)) + 1);
-for i = 0:90
-
-upperLeg.theta0 = i * pi / 180;
-q0(2) = 0;
-
 %% Linearization
 % Linearization about equilibrium (q0, q_dot0). Use symbolic q0,q_dot0 or numeric later.
 % Balance at center of mass angle instead of payload angle
@@ -223,14 +217,17 @@ tic
 Q = diag(((1 ./ [wr(1)*0.05 0.01 wr(1)*0.2 0.2]) .^ 2));
 R = diag(((1 ./ nonzeros(u_max)) .^ 2));
 
-K(:,:,i+1) = lqr(A_lin, B_lin, Q, R); % N = 0
-
+K = lqr(A_lin, B_lin, Q, R); % N = 0
+K_exp = [[-20 0 0 0]; ...
+         [2.06715687e+00 3.09321227e-02 1.19677910e+00 2.23678771e+03]; ...
+         [ -0.10793587   0.03065276  -0.84218993 -30.45880943]; ...
+         [2.09840433e+00 3.18031754e-02 1.25890809e+00 1.97956957e+02]];
+         
 disp('For Upper Leg theta:');
 disp(upperLeg.theta0 * 180 / pi)
 disp('LQR Gain Matrix K:');
 disp(K);
 
-end
 toc
 
 %% Simulate closed-loop system
